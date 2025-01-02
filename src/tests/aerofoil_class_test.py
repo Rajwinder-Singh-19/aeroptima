1da62cc5df1f7a48e6e04852b0044f6d8a07e982
Rajwinder-Singh-19 <i.am.rajwinder.19@gmail.com>
2025-01-02 23:17:09 +0530

    Formatting code

--- src/tests/aerofoil_class_test.py
+++ src/tests/aerofoil_class_test.py
@@ -2,18 +2,43 @@ if __name__ == "__main__":
     from database.UIUC_aerofoils import UIUC_DATABASE as UDB
     from classes.aerofoil import Aerofoil
     import matplotlib.pyplot as plt
-    bambino_6 = Aerofoil(UDB['bambino6_dat'], n_segments=5)
-    bambino_6_upper = bambino_6.getUpperCurve(30)
-    bambino_6_lower = bambino_6.getLowerCurve(30)    
 
-    plt.plot(bambino_6.upper_coords[:, 0], bambino_6.upper_coords[:, 1], c="red", label="Original Upper Surface") 
-    plt.plot(bambino_6.lower_coords[:, 0], bambino_6.lower_coords[:, 1], c="red", label="Original Lower Surface")     
-    plt.plot(bambino_6_upper[:, 0], bambino_6_upper[:, 1], c="blue", label="Upper Bezier")
-    plt.plot(bambino_6_lower[:, 0], bambino_6_lower[:, 1], c="orange", label="Lower Bezier")
-    plt.scatter(bambino_6.upper_control[:, 0], bambino_6.upper_control[:, 1], c="blue", label="Upper Control Points")
-    plt.scatter(bambino_6.lower_control[:, 0], bambino_6.lower_control[:, 1], c="orange", label="Lower Control Points")    
+    bambino_6 = Aerofoil(UDB["bambino6_dat"], n_segments=5)
+    bambino_6_upper = bambino_6.getUpperCurve(30)
+    bambino_6_lower = bambino_6.getLowerCurve(30)
+
+    plt.plot(
+        bambino_6.upper_coords[:, 0],
+        bambino_6.upper_coords[:, 1],
+        c="red",
+        label="Original Upper Surface",
+    )
+    plt.plot(
+        bambino_6.lower_coords[:, 0],
+        bambino_6.lower_coords[:, 1],
+        c="red",
+        label="Original Lower Surface",
+    )
+    plt.plot(
+        bambino_6_upper[:, 0], bambino_6_upper[:, 1], c="blue", label="Upper Bezier"
+    )
+    plt.plot(
+        bambino_6_lower[:, 0], bambino_6_lower[:, 1], c="orange", label="Lower Bezier"
+    )
+    plt.scatter(
+        bambino_6.upper_control[:, 0],
+        bambino_6.upper_control[:, 1],
+        c="blue",
+        label="Upper Control Points",
+    )
+    plt.scatter(
+        bambino_6.lower_control[:, 0],
+        bambino_6.lower_control[:, 1],
+        c="orange",
+        label="Lower Control Points",
+    )
     plt.legend()
     plt.title("Bambino 6 Aerofoil Object Visualized")
     plt.xlim((-1, 2))
-    plt.ylim((-1, 1))    
+    plt.ylim((-1, 1))
     plt.show()
